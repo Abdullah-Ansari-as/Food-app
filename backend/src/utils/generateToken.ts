82b1061e563691930b0cc2591e33175583f3ac5c
Abdulllah <yousaf.8666305@gmail.com>
2025-06-19 10:46:52 +0500

    fix jwt for production

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -3,7 +3,19 @@ import jwt from "jsonwebtoken";
 import { IUserDocument } from "../models/user-model";
 
 export const generateToken = (res: Response, user: IUserDocument) => {
+	const isProduction = process.env.NODE_ENV === "production";
 	const token = jwt.sign({userId: user?._id}, process.env.TOKEN_SECRET_KEY!, {expiresIn: "1d"});
-	res.cookie("token", token, {httpOnly: true, sameSite: "strict", maxAge: 24 * 60 * 60 * 1000});
+
+	// ********* this is recomended for production
+	res.cookie("token", token, {
+				httpOnly: true,
+				sameSite: isProduction ? "none" : "lax",
+				secure: isProduction, // only send over HTTPS
+				maxAge: 24 * 60 * 60 * 1000,
+			})
+
+	// ********** token problem with this in production
+	// res.cookie("token", token, {httpOnly: true, sameSite: "strict", maxAge: 24 * 60 * 60 * 1000});
+	
 	return token;
 };
